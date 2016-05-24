8ac3695d75a5a2439a0b26e750d58108b7422e08
edouardfouche <edouard.fouche@hotmail.fr>
2016-05-24 07:30:48 +0200

    NNPraktikum 2

--- src/Run.py
+++ src/Run.py
@@ -2,45 +2,68 @@
 # -*- coding: utf-8 -*-
 
 from data.mnist_seven import MNISTSeven
-#from model.stupid_recognizer import StupidRecognizer
+from model.stupid_recognizer import StupidRecognizer
 from model.perceptron import Perceptron
-#from model.logistic_regression import LogisticRegression
+from model.logistic_regression import LogisticRegression
 from report.evaluator import Evaluator
 
 
-def main():
-    data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
-    #    myStupidClassifier = StupidRecognizer(data.trainingSet,
-    #                                          data.validationSet,
-    #                                          data.testSet)
-    # Uncomment this to make your Perceptron evaluated
-    myPerceptronClassifier = Perceptron(data.trainingSet,
-                                        data.validationSet,
-                                        data.testSet,
-                                        learningRate=0.005,  # 0.005
-                                        epochs=100) # 30
-    
-    # Train the classifiers
-    print("=========================")
-    print("Training..")
-    
-    print("\nStart training the Perceptron..")
-    myPerceptronClassifier.train()
-    print("Done..")
-    
-    # Do the recognizer
-    # Explicitly specify the test set to be evaluated
-    perceptronPred = myPerceptronClassifier.evaluate()
-    
-    # Report the result
-    print("=========================")
-    print("Evaluation..")
-    evaluator = Evaluator()
-    
-    print("\nResult of the Perceptron recognizer:")
-    evaluator.printAccuracy(data.testSet, perceptronPred)
-    evaluator.printConfusionMatrix(data.testSet, perceptronPred)
-    evaluator.printClassificationResult(data.testSet, perceptronPred, None)
+#def main():
 
-if __name__ == '__main__':
-    main()
+data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
+#myStupidClassifier = StupidRecognizer(data.trainingSet,
+#                                      data.validationSet,
+#                                      data.testSet)
+#myPerceptronClassifier = Perceptron(data.trainingSet,
+#                                    data.validationSet,
+#                                    data.testSet,
+#                                    learningRate=0.005,
+#                                    epochs=10)
+# Uncomment this to run Logistic Neuron Layer
+myLRClassifier = LogisticRegression(data.trainingSet,
+                                    data.validationSet,
+                                    data.testSet,
+                                    learningRate=0.002,
+                                    epochs=500)
+
+# Train the classifiers
+print("=========================")
+print("Training..")
+
+#print("\nStupid Classifier has been training..")
+#myStupidClassifier.train()
+#print("Done..")
+
+#print("\nPerceptron has been training..")
+#myPerceptronClassifier.train()
+#print("Done..")
+
+print("\nLogistic Regression has been training..")
+myLRClassifier.train()
+print("Done..")
+
+# Do the recognizer
+# Explicitly specify the test set to be evaluated
+#stupidPred = myStupidClassifier.evaluate()
+#perceptronPred = myPerceptronClassifier.evaluate()
+lrPred = myLRClassifier.evaluate()
+
+# Report the result
+print("=========================")
+evaluator = Evaluator()
+
+#print("Result of the stupid recognizer:")
+# evaluator.printComparison(data.testSet, stupidPred)
+#evaluator.printAccuracy(data.testSet, stupidPred)
+
+#print("\nResult of the Perceptron recognizer:")
+# evaluator.printComparison(data.testSet, perceptronPred)
+#evaluator.printAccuracy(data.testSet, perceptronPred)
+
+print("\nResult of the Logistic Regression recognizer:")
+#evaluator.printComparison(data.testSet, perceptronPred)
+evaluator.printAccuracy(data.testSet, lrPred)
+
+
+#if __name__ == '__main__':
+#    main()
