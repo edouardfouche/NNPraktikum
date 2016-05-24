f17db7d11744d8a0d40816a70a378cf873699aee
edouardfouche <edouard.fouche@hotmail.fr>
2016-05-24 11:12:09 +0200

    Add visualization

--- src/Run.py
+++ src/Run.py
@@ -1,69 +1,60 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from bokeh.plotting import figure, output_file, show
+import pandas
+    
 from data.mnist_seven import MNISTSeven
-from model.stupid_recognizer import StupidRecognizer
-from model.perceptron import Perceptron
+#from model.stupid_recognizer import StupidRecognizer
+#from model.perceptron import Perceptron
 from model.logistic_regression import LogisticRegression
 from report.evaluator import Evaluator
 
-
-#def main():
-
-data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
-#myStupidClassifier = StupidRecognizer(data.trainingSet,
-#                                      data.validationSet,
-#                                      data.testSet)
-#myPerceptronClassifier = Perceptron(data.trainingSet,
-#                                    data.validationSet,
-#                                    data.testSet,
-#                                    learningRate=0.005,
-#                                    epochs=10)
-# Uncomment this to run Logistic Neuron Layer
-myLRClassifier = LogisticRegression(data.trainingSet,
-                                    data.validationSet,
-                                    data.testSet,
-                                    learningRate=0.002,
-                                    epochs=500)
-
-# Train the classifiers
-print("=========================")
-print("Training..")
-
-#print("\nStupid Classifier has been training..")
-#myStupidClassifier.train()
-#print("Done..")
-
-#print("\nPerceptron has been training..")
-#myPerceptronClassifier.train()
-#print("Done..")
-
-print("\nLogistic Regression has been training..")
-myLRClassifier.train()
-print("Done..")
-
-# Do the recognizer
-# Explicitly specify the test set to be evaluated
-#stupidPred = myStupidClassifier.evaluate()
-#perceptronPred = myPerceptronClassifier.evaluate()
-lrPred = myLRClassifier.evaluate()
-
-# Report the result
-print("=========================")
-evaluator = Evaluator()
-
-#print("Result of the stupid recognizer:")
-# evaluator.printComparison(data.testSet, stupidPred)
-#evaluator.printAccuracy(data.testSet, stupidPred)
-
-#print("\nResult of the Perceptron recognizer:")
-# evaluator.printComparison(data.testSet, perceptronPred)
-#evaluator.printAccuracy(data.testSet, perceptronPred)
-
-print("\nResult of the Logistic Regression recognizer:")
-#evaluator.printComparison(data.testSet, perceptronPred)
-evaluator.printAccuracy(data.testSet, lrPred)
+def main():
+    learningRate=0.05
+    epochs=500
+    
+    data = MNISTSeven("../data/mnist_seven.csv", 3000, 1000, 1000)
+    
+    myLRClassifier = LogisticRegression(data.trainingSet,
+                                        data.validationSet,
+                                        data.testSet,
+                                        learningRate=learningRate,
+                                        epochs=epochs)
+    
+    # Train the classifiers
+    print("=========================")
+    print("Training..")
+    
+    
+    print("\nLogistic Regression training..")
+    result_training, result_validation = myLRClassifier.train()
+    print("Done..")
+    
+    lrPred = myLRClassifier.evaluate()
+    
+    # Report the result
+    print("=========================")
+    evaluator = Evaluator()
+    
+    
+    print("\nResult of the Logistic Regression recognizer:")
+    #evaluator.printComparison(data.testSet, lrPred)
+    evaluator.printAccuracy(data.testSet, lrPred)
+    
+    visualize(result_training, result_validation, epochs, learningRate)
 
 
-#if __name__ == '__main__':
-#    main()
+def visualize(result_training, result_validation, epochs, learningRate):
+    title = "plot_%sepoches_lr%s.html"%(epochs,str(learningRate).replace(".",","))
+    output_file(title)
+    p = figure(width=800, height=800, title="Logistic neuron with %s epochs, learning rate = %s"%(epochs,learningRate))
+    data_t = pandas.DataFrame.from_dict(result_training, orient="index")
+    data_v = pandas.DataFrame.from_dict(result_validation, orient="index")
+    
+    p.line(list(data_t.index), data_t[0], color="red", legend="training")
+    p.line(list(data_v.index), data_v[0], color="blue", legend="validation")
+    show(p)
+
+if __name__ == '__main__':
+    main()
